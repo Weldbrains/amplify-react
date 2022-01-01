426155ad7318dc6374fee047bfce86b1755bbe5a
Mohammad Yaghoubi <m.yaghoubi@web.de>
2022-01-01 12:26:36 +0100

    v1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn The Future :)
         </a>
       </header>
     </div>
